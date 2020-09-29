666ad5160de48cca4a010636a43ea51a086b6685
samir0067 <arrahmani.samir@gmail.com>
2020-09-29 11:40:04 +0200

    first commit

---
+++ src/components/AdminForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const AdminForm = ({ id: key, majRecette, recettes, supprimerRecette }) => {
+  const recette = recettes[key]
+
+  const handleChange = (event, key) => {
+    const { name, value } = event.target
+    const recette = recettes[key]
+    recette[name] = value
+    majRecette(key, recette)
+  }
+
+  return (
+    <div className='card'>
+      <form className='admin-form'>
+        <input value={recette.nom} onChange={e => handleChange(e, key)}
+               name='nom' type='text' placeholder='Nom de la recette'/>
+
+        <input value={recette.image} onChange={e => handleChange(e, key)}
+               name='image' type='text' placeholder={'Nom de l\'image'}/>
+
+        <textarea value={recette.ingredients} onChange={e => handleChange(e, key)}
+                  name='ingredients' rows='3' placeholder='Liste des ingredients'/>
+
+        <textarea value={recette.instructions} onChange={e => handleChange(e, key)}
+                  name='instructions' rows='15' placeholder='Liste des instructions'/>
+      </form>
+      <button onClick={() => supprimerRecette(key)}>Supprimer la recette</button>
+    </div>
+  )
+}
+
+export default AdminForm
